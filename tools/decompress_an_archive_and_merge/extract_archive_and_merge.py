43458247eac124200574d29b09592940c0837af3
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2018-04-06 11:50:03 +0200

    flake8

--- tools/decompress_an_archive_and_merge/extract_archive_and_merge.py
+++ tools/decompress_an_archive_and_merge/extract_archive_and_merge.py
@@ -10,122 +10,128 @@ And with the help of Anthony Bretaudeau for some stuff with bz2.
 
 """
 
-import argparse, os, sys, subprocess, tempfile, shutil, gzip, zipfile, tarfile, gzip, bz2, shutil
-import glob 
-from galaxy import eggs
-from galaxy import util
-from galaxy.datatypes.checkers import *
+import bz2
+import glob
+import gzip
+import os
+import shutil
+import sys
+import tarfile
+import zipfile
 
-def stop_err( msg ):
-    sys.stderr.write( '%s\n' % msg )
+from galaxy.datatypes.checkers import check_bz2, check_gzip, check_zip
+
+
+def stop_err(msg):
+    sys.stderr.write('%s\n' % msg)
     sys.exit()
+
 
 def main(archive, archivename, logfile, logid, workdir, merge, rm_header=0, concat=''):
 
     # create a temporary repository
-    #tmp_dir = tempfile.mkdtemp(dir=os.getcwd())
-    os.mkdir("decompress_files")    
+    # tmp_dir = tempfile.mkdtemp(dir=os.getcwd())
+    os.mkdir("decompress_files")
 
-    #open log file
-    mylog = open(logfile, "w");
+    # open log file
+    mylog = open(logfile, "w")
 
-    is_gzipped, is_gzvalid = check_gzip( archive )
-    is_bzipped, is_bzvalid = check_bz2( archive )
+    is_gzipped, is_gzvalid = check_gzip(archive)
+    is_bzipped, is_bzvalid = check_bz2(archive)
 
     # extract all files in a temp directory
     # test if is a zip file
-    if check_zip( archive ):
-	with zipfile.ZipFile(archive, 'r') as myarchive:
-		myarchive.extractall("decompress_files")
+    if check_zip(archive):
+        with zipfile.ZipFile(archive, 'r') as myarchive:
+            myarchive.extractall("decompress_files")
 
     # test if is a tar file
-    elif tarfile.is_tarfile( archive ):
-	mytarfile=tarfile.TarFile.open(archive)
-	mytarfile.extractall("decompress_files")
-	mytarfile.close()
- 
+    elif tarfile.is_tarfile(archive):
+        mytarfile = tarfile.TarFile.open(archive)
+        mytarfile.extractall("decompress_files")
+        mytarfile.close()
+
     # test if is a gzip file
-    elif is_gzipped and is_gzvalid :
-	mygzfile = gzip.open(archive, 'rb')
+    elif is_gzipped and is_gzvalid:
+        mygzfile = gzip.open(archive, 'rb')
 
-	myungzippedfile = open ("decompress_files/"+os.path.splitext(os.path.basename(archivename))[0], 'wb', 2**20)
-	for i in iter(lambda: mygzfile.read(2**20), ''):
-		myungzippedfile.write(i) 
+        myungzippedfile = open("decompress_files/" + os.path.splitext(os.path.basename(archivename))[0], 'wb', 2**20)
+        for i in iter(lambda: mygzfile.read(2**20), ''):
+            myungzippedfile.write(i)
 
-	myungzippedfile.close()
-	mygzfile.close()
+        myungzippedfile.close()
+        mygzfile.close()
 
     elif is_bzipped and is_bzvalid:
         mybzfile = bz2.BZ2File(archive, 'rb')
 
-        myunbzippedfile = open ("decompress_files/"+os.path.splitext(os.path.basename(archivename))[0], 'wb', 2**20)
+        myunbzippedfile = open("decompress_files/" + os.path.splitext(os.path.basename(archivename))[0], 'wb', 2**20)
         for i in iter(lambda: mybzfile.read(2**20), ''):
                 myunbzippedfile.write(i)
 
         myunbzippedfile.close()
         mybzfile.close()
 
-		
     # test if merge is enable
     if merge == "true":
-	mylog.write("Merge option is enabled with "+str(rm_header)+" lines to deleted\n\n")
-	myfinalfile = open(concat, "w");
-	for myfile in listdirectory("decompress_files"):
-		myopenfile = open(myfile, "r")
-		nblinesremove=0
-		mylog.write(os.path.basename(myfile)+" is extracted from the archive and is added into the result file\n")
-		for line in myopenfile:	
+        mylog.write("Merge option is enabled with " + str(rm_header) + " lines to deleted\n\n")
+        myfinalfile = open(concat, "w")
+        for myfile in listdirectory("decompress_files"):
+            myopenfile = open(myfile, "r")
+            nblinesremove = 0
+            mylog.write(os.path.basename(myfile) + " is extracted from the archive and is added into the result file\n")
+            for line in myopenfile:
 
-			#if not equal, don't write	
-			if int(rm_header) != nblinesremove:
-				nblinesremove=nblinesremove+1
-			else:
-				# write the line into the final file			
-				myfinalfile.write(line)
-	
-	myfinalfile.close()
+                # if not equal, don't write
+                if int(rm_header) != nblinesremove:
+                    nblinesremove = nblinesremove + 1
+                else:
+                    # write the line into the final file
+                    myfinalfile.write(line)
 
-	shutil.rmtree("decompress_files")
+        myfinalfile.close()
+
+        shutil.rmtree("decompress_files")
 
     else:
-	# if merge is disable
+        # if merge is disable
         mylog.write("Merge option is disabled\n\n")
 
-   	# move all files (recursively) in the working dir
-    	for myfile in listdirectory("decompress_files"):
-		myfileclean = myfile.replace(" ", "\ ")
+        # move all files (recursively) in the working dir
+        for myfile in listdirectory("decompress_files"):
+            myfileclean = myfile.replace(" ", "\ ")
 
-		mylog.write(os.path.basename(myfileclean)+" is extracted from the archive \n")
+            mylog.write(os.path.basename(myfileclean) + " is extracted from the archive \n")
 
-		fileext = os.path.splitext(myfile)[1].replace(".", "")
+            fileext = os.path.splitext(myfile)[1].replace(".", "")
 
-		# if no extension
-		if fileext == '':
-                        shutil.move(os.path.abspath(myfile), os.path.abspath(myfile)+".txt")
+            # if no extension
+            if fileext == '':
+                shutil.move(os.path.abspath(myfile), os.path.abspath(myfile) + ".txt")
 
-		if fileext == 'fa':
-                        shutil.move(os.path.abspath(myfile), os.path.abspath(myfile).replace(".fa", ".fasta"))
+            if fileext == 'fa':
+                shutil.move(os.path.abspath(myfile), os.path.abspath(myfile).replace(".fa", ".fasta"))
 
-		if fileext == 'fq':
-			shutil.move(os.path.abspath(myfile), os.path.abspath(myfile).replace(".fq", ".fastq"))
+            if fileext == 'fq':
+                shutil.move(os.path.abspath(myfile), os.path.abspath(myfile).replace(".fq", ".fastq"))
 
-   	mylog.write("\nPlease refresh your history if all files are not present\n")
-    	mylog.close()
-
+        mylog.write("\nPlease refresh your history if all files are not present\n")
+        mylog.close()
 
 
 # parse the directory and return files path (in a tab)
 def listdirectory(path):
-	myfile=[]
-	l = glob.glob(path+'/*') 
-    	for i in l:
-		# if directory
-        	if os.path.isdir(i): 
-			myfile.extend(listdirectory(i))
-		# else put the file in the tab      	  	
-		else:
-			myfile.append(i)
-    	return myfile
+    myfile = []
+    dirlist = glob.glob(path + '/*')
+    for i in dirlist:
+        # if directory
+        if os.path.isdir(i):
+            myfile.extend(listdirectory(i))
+        # else put the file in the tab
+        else:
+            myfile.append(i)
+    return myfile
 
 
-if __name__=="__main__": main(*sys.argv[1:])
+if __name__ == "__main__":
+    main(*sys.argv[1:])
--- tools/ziptool/zip.py
+++ tools/ziptool/zip.py
@@ -8,25 +8,23 @@ usage: zip.py [options]
 See below for options
 """
 
-import os, subprocess, sys
+import os
+import sys
 import zipfile
+
 
 def __main__():
 
-	# init
-	output_zip = sys.argv[1]
+    # init
+    output_zip = sys.argv[1]
 
-	# create the zip archive
-	myarchive = zipfile.ZipFile(output_zip, 'w', allowZip64=True)
+    # create the zip archive
+    myarchive = zipfile.ZipFile(output_zip, 'w', allowZip64=True)
 
-	# for all files, write in the archive
-	for i in range(2, len(sys.argv),2):
-		myarchive.write(sys.argv[i], os.path.basename(sys.argv[i+1]))
-
-
+    # for all files, write in the archive
+    for i in range(2, len(sys.argv), 2):
+        myarchive.write(sys.argv[i], os.path.basename(sys.argv[i + 1]))
 
 
-
-
-if __name__ == "__main__": __main__()
-
+if __name__ == "__main__":
+    __main__()
